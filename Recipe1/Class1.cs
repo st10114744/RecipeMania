042be8c7c2a7f9c0a638aad76218a694e2f40a3c
Aidan Kollmann <st10114744@vcconnect.edu.za>
2023-05-08 13:06:24 +0200

    Add project files.

---
+++ Recipe1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe1
+{
+    internal class Class1
+    {
+        public Class1() 
+        {
+        
+        }
+
+        private void Intro() 
+        { 
+
+        
+        }
+
+    }
+}
